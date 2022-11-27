7efb197decc0d90ab2329c067f0d564f7eed404b
zorro901 <zoro907@gmail.com>
2022-11-28 02:53:44 +0900

    :lipstick: style(next.config): イメージソースとStrictModeを追加

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   experimental: {
     appDir: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'imageio.forbes.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.reuters.com',
+      },
+    ],
   },
 }
 
