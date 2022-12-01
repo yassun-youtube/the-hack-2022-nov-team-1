0ef9918587ce4472c131b83f170ae97ece9117b2
zorro901 <zoro907@gmail.com>
2022-12-01 22:22:35 +0900

    :sparkles: feat: heroSectionを作成しました

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import HeroSection from '@components/organisms/HeroSection'
 import TextBarSection from '@components/organisms/TextBarSection'
 import TopPageNewsSection from '@components/organisms/TopPageNewsSection'
 
 export default function Home() {
   return (
     <>
+      <HeroSection />
       <TextBarSection />
       <TopPageNewsSection />
     </>
