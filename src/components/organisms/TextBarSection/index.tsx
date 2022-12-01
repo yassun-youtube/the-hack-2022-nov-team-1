ff40b0288d606f8b49f872b2e17698dbd579393d
zorro901 <zoro907@gmail.com>
2022-12-01 22:23:43 +0900

    :lipstick: style: textBarSectionのwidth変更

--- src/components/organisms/TextBarSection/index.tsx
+++ src/components/organisms/TextBarSection/index.tsx
@@ -2,7 +2,7 @@ import TextBarListItem from '@components/molecules/TextBarListItem'
 
 const TextBarSection = () => {
   return (
-    <section className={'w-screen '}>
+    <section className={'w-full'}>
       <ul className={'mx-10 flex justify-evenly border-t-2 border-t-blue/10'}>
         <TextBarListItem
           text={'検視官 アイダホ州の学生はベッドで刺殺された'}
