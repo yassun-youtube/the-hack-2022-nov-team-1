f3e6be4fbf3f578fe13323343ec823a204e567b9
zorro901 <zoro907@gmail.com>
2022-12-06 16:29:48 +0900

    :lipstick: style(head): titleを変更

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Create Next App</title>
+      <title>steadfast-news</title>
       <meta
         content='width=device-width, initial-scale=1'
         name='viewport'
