7f767224dc1b8699af380ec298d495664b3c20bb
こーざき <seito_kozaki@hotmail.com>
2022-12-10 18:12:21 +0900

    :bug: fix: sideNewsTextのフラグメントの指定方法を変更

--- src/components/atoms/SideNewsText/index.tsx
+++ src/components/atoms/SideNewsText/index.tsx
@@ -1,17 +1,15 @@
-import React from "react"
-
 type SideNewsTextComponent = {
   text: string
 }
 
 const SideNewsText = ({ text }: SideNewsTextComponent) => {
   return (
-    <React.Fragment>
+    <>
       <div className={`h-32 bg-zinc pl-0.5 pr-16 pt-10`}>
         <span className={`font-bold`}>{text}</span>
       </div>
       <hr className={`w-16 text-lightgray`}></hr>
-    </React.Fragment>
+    </>
   )
 }
 
