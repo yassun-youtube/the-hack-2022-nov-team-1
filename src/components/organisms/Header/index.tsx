ebee970c053081206a26c0304793684a956ba05e
zorro901 <zoro907@gmail.com>
2022-12-15 07:40:45 +0900

    :bug: fix(Header): トップページへのリンクを追加

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import HeaderNavContainer from '@components/molecules/HeaderNavContainer'
 import SearchField from '@components/molecules/SearchField'
 import { HEADER_NAV } from 'constant/constant'
@@ -5,10 +6,12 @@ import { HEADER_NAV } from 'constant/constant'
 const HeaderSection = () => {
   return (
     <header className='mx-10 h-[134px] pt-7 pb-5'>
-      <div className='mb-7 flex items-center'>
-        <h2 className='mr-2 bg-red text-2xl font-bold text-white'>steadfast</h2>
-        <h2 className='text-2xl font-bold'>news</h2>
-      </div>
+      <Link href={'/'}>
+        <div className='mb-7 flex items-center'>
+          <h2 className='mr-2 bg-red text-2xl font-bold text-white'>steadfast</h2>
+          <h2 className='text-2xl font-bold'>news</h2>
+        </div>
+      </Link>
       <div className='flex items-center justify-between font-sans font-semibold text-black'>
         <ul className='flex space-x-5'>
           {HEADER_NAV.map((item) => (
