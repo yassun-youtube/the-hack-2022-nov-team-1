717b268d713810ec1798b8a2d3c6d0b225a51868
zorro901 <zoro907@gmail.com>
2022-12-16 14:26:42 +0900

    :bug: fix: リンクの定数を変更

--- src/constant/constant.ts
+++ src/constant/constant.ts
@@ -2,15 +2,15 @@ import { HeaderNavType } from 'types/header'
 
 export const LIST_TEXT_ARR = [
   {
-    label: 'AP NEWS',
+    label: 'steadfast news',
     content: [
       {
-        text: 'string',
-        link: 'https://example.com/',
+        text: 'Top Stories',
+        link: '/',
       },
       {
-        text: 'sssss',
-        link: 'https://example.com/',
+        text: 'Video',
+        link: '/',
       },
     ],
   },
@@ -18,12 +18,12 @@ export const LIST_TEXT_ARR = [
     label: 'MORE FORM APP',
     content: [
       {
-        text: 'AAAA',
-        link: 'https://example.com/',
+        text: 'sf.org',
+        link: '/',
       },
       {
-        text: 'CCCCCC',
-        link: 'http://example.com/',
+        text: 'next.js',
+        link: '/',
       },
     ],
   },
@@ -31,59 +31,32 @@ export const LIST_TEXT_ARR = [
 
 export const FOOTER_NAV = [
   {
-    label: 'THE ASSOCIATED PRESS',
-    link: 'http://example.com/',
+    label: 'steadfast news',
+    link: '/',
   },
   {
     label: 'About',
-    link: 'http://example.com/',
+    link: '/',
   },
   {
     label: 'Contact',
-    link: 'http://example.com/',
+    link: '/',
   },
   {
     label: 'Customer Support',
-    link: 'http://example.com/',
+    link: '/',
   },
   {
     label: 'Careers',
-    link: 'http://example.com/',
+    link: '/',
   },
   {
     label: 'Teams & Conditions',
-    link: 'http://example.com/',
+    link: '/',
   },
   {
     label: 'Privacy',
-    link: 'http://example.com/',
-  },
-]
-
-export const SNS = [
-  // twitter
-  {
-    link: 'https://twitter.com/',
-    imgPath: '/twitter.png',
-    alt: 'twitter',
-  },
-  // facebook
-  {
-    link: 'https://ja-jp.facebook.com//',
-    imgPath: '/facebook.png',
-    alt: 'twitter',
-  },
-  // youtube
-  {
-    link: 'https://www.youtube.com/',
-    imgPath: '/youtube.png',
-    alt: 'twitter',
-  },
-  // instagram
-  {
-    link: 'https://www.instagram.com/',
-    imgPath: '/instagram.webp',
-    alt: 'twitter',
+    link: '/',
   },
 ]
 
