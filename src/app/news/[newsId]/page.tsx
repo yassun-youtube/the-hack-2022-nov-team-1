5c7ebe560a0d2e2f446b4ca819c8b180d5a94c8c
zorro901 <zoro907@gmail.com>
2022-12-17 21:07:35 +0900

    :zap: perf(SSG): 詳細ページにSSGを設定

--- src/app/news/[newsId]/page.tsx
+++ src/app/news/[newsId]/page.tsx
@@ -1,21 +1,30 @@
 import NewsDetailPageClient from '@components/templates/NewsDetailPage'
-import { getSpecificNews } from '@libs/cmsUtils'
-import { getTweets } from '@libs/tweetUtil'
+import { getSpecificNews, getLatestNewsIds } from '@libs/cmsUtils'
+import { getTweetsCached, preloadTweets } from '@libs/tweetUtil'
 import { newsItem } from 'types/newsItem'
 
 type NewsDetailPage = {
   params: { newsId: string }
 }
 
-const NewsDetailPage = async ({ params }: NewsDetailPage) => {
-  const newsItem: newsItem = await getSpecificNews(params.newsId)
-  const tweetItems: string[] = await getTweets(newsItem.originalUrl)
+const NewsDetailPage = async ({ params: { newsId } }: NewsDetailPage) => {
+  const newsItem: newsItem = await getSpecificNews(newsId)
+  preloadTweets(newsItem.originalUrl)
+  const tweetItems: string[] = await getTweetsCached(newsItem.originalUrl)
   return (
     <NewsDetailPageClient
       newsItem={newsItem}
+      newsId={newsId}
       tweetItems={tweetItems}
     />
   )
 }
 
 export default NewsDetailPage
+
+export const generateStaticParams = async () => {
+  const newsItems: newsItem[] = await getLatestNewsIds()
+  return newsItems.map((newsItem) => ({
+    newsId: newsItem.id,
+  }))
+}
