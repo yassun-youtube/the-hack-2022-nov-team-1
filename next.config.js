53e111d0ebfd8314408e768a8f2516f9079ab004
zorro901 <zoro907@gmail.com>
2022-12-17 18:15:46 +0900

    :package: build: package更新

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
+    fontLoaders: [{ loader: '@next/font/google', options: { subsets: ['latin'] } }],
   },
   images: {
     remotePatterns: [
