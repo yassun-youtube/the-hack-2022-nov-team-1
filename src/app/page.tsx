5bbdb725af2f5b3f2b3175211ac6a50456888b2e
zorro901 <zoro907@gmail.com>
2022-12-17 17:48:04 +0900

    :sparkles: feat(CategoryNewsSection): categoryNewsSectionを実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,43 @@
+import { Suspense } from 'react'
 import ScrollManagement from '@components/container/scrollManagement'
+import CategoryNewsSection from '@components/organisms/CategoryNewsSection'
 import HeroSection from '@components/organisms/HeroSection'
 import TextBarSection from '@components/organisms/TextBarSection'
-import TopPageNewsSection from '@components/organisms/TopPageNewsSection'
-import { getLatestNews } from '@libs/cmsUtils'
+import { getFilteredNews, getLatestNews } from '@libs/cmsUtils'
+import { newsItem } from 'types/newsItem'
 
 export default async function Home() {
   const news = await getLatestNews()
+  const technologyNewsItems: newsItem[] = await getFilteredNews('technology')
+  const scienceNewsItems: newsItem[] = await getFilteredNews('science')
+  const businessNewsItems: newsItem[] = await getFilteredNews('business')
+  const politicsNewsItems: newsItem[] = await getFilteredNews('politics')
   return (
     <>
       <ScrollManagement />
-      <HeroSection
-        newsItem={news[0]}
-        sideNewsItems={news.slice(1, 5)}
-      />
-      <TextBarSection newsItems={news.slice(1, 6)} />
-      <TopPageNewsSection
-        featureNewsItem={news.slice(7, 8)}
-        newsItems={news.slice(5)}
-      />
+      <Suspense fallback={<p>Loading news...</p>}>
+        <HeroSection
+          newsItem={news[0]}
+          sideNewsItems={news.slice(1, 5)}
+        />
+        <TextBarSection newsItems={news.slice(1, 6)} />
+        <CategoryNewsSection
+          categoryName={'technology'}
+          newsItems={technologyNewsItems}
+        />
+        <CategoryNewsSection
+          categoryName={'science'}
+          newsItems={scienceNewsItems}
+        />
+        <CategoryNewsSection
+          categoryName={'business'}
+          newsItems={businessNewsItems}
+        />
+        <CategoryNewsSection
+          categoryName={'politics'}
+          newsItems={politicsNewsItems}
+        />
+      </Suspense>
     </>
   )
 }
--- src/components/organisms/CategoryNewsSection/index.tsx
+++ src/components/organisms/CategoryNewsSection/index.tsx
@@ -3,34 +3,37 @@ import ListItemContainer from '@components/molecules/ListItemContainer'
 import { newsItem } from 'types/newsItem'
 
 type TopPageNewsSectionProps = {
-  featureNewsItem: newsItem[]
   newsItems: newsItem[]
+  categoryName: string
 }
 
-const TopPageNewsSection = ({ featureNewsItem, newsItems }: TopPageNewsSectionProps) => {
+const CategoryNewsSection = ({ categoryName, newsItems }: TopPageNewsSectionProps) => {
   return (
     <section className={'flex justify-center'}>
       <div className={'my-24 h-[409px] w-[680px] bg-white'}>
         <div>
           <hr className={'mb-6 border-t-8 border-t-gray'} />
           <h1 className={'font-robot text-2xl font-bold leading-7 tracking-[0.03em] text-blue'}>
-            TECHNOLOGY
+            {categoryName.toUpperCase()}
           </h1>
           <div className={'mt-6 flex'}>
             <ImageContainer
-              url={`news/${featureNewsItem[0].id}`}
-              paragraph={featureNewsItem[0].title}
-              alt={featureNewsItem[0].description}
-              src={featureNewsItem[0].imageSrc}
+              url={`news/${newsItems[0].id}`}
+              paragraph={newsItems[0].title}
+              alt={newsItems[0].description}
+              src={newsItems[0].imageSrc}
             />
             <ul className={'ml-10 flex-col'}>
-              {newsItems.map((newsItem) => (
-                <ListItemContainer
-                  key={newsItem.id}
-                  url={`news/${newsItem.id}`}
-                  title={newsItem.title}
-                />
-              ))}
+              {newsItems.map((newsItem, index) => {
+                if (index !== 0 && index <= 5)
+                  return (
+                    <ListItemContainer
+                      key={newsItem.id}
+                      url={`news/${newsItem.id}`}
+                      title={newsItem.title}
+                    />
+                  )
+              })}
             </ul>
           </div>
         </div>
@@ -39,4 +42,4 @@ const TopPageNewsSection = ({ featureNewsItem, newsItems }: TopPageNewsSectionPr
   )
 }
 
-export default TopPageNewsSection
+export default CategoryNewsSection
