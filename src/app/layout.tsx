aa455d1e3fee939f6fb96b8f86f7cf9869edb2f6
zorro901 <zoro907@gmail.com>
2023-03-09 19:59:14 +0900

    :hammer: chore: Next/fontを比活性

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,20 @@
 import '../styles/dist.css'
-import { Roboto } from '@next/font/google'
 import Footer from '@components/organisms/Footer'
 import Header from '@components/organisms/Header'
 
-const roboto = Roboto({
-  variable: '--roboto-font',
-  weight: '100',
-})
+// TODO:turbopack next/font 対応待ち
+// const roboto = Roboto({
+//   variable: '--roboto-font',
+//   weight: '100',
+// })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html
       lang='en'
-      className={roboto.variable}>
+      // TODO:turbopack next/font 対応待ち
+      // className={roboto.variable}
+    >
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
